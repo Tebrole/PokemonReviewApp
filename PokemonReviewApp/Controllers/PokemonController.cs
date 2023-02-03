89083b8b0e44360a12562753cd74d65593397989
Tekla Ptskialadze <tekla.ptskialadze@gmail.com>
2023-02-03 23:50:43 +0400

    Add project files.

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return Ok(pokemons);
+        }
+
+        [HttpGet("{Id}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(400)]
+
+        public IActionResult GetPokemon(int id) 
+        {
+            if (!_pokemonRepository.PokemonExists(id))
+            {
+                return NotFound();
+            }
+            var pokemon = _pokemonRepository.GetPokemon(id);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet("{Id}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+
+        public IActionResult GetPokemonRating(int id)
+        {
+            if (!_pokemonRepository.PokemonExists(id))
+            {
+                return NotFound();
+            }
+            var rating = _pokemonRepository.GetPokemonRating(id);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(rating);
+        }
+    }
+}
